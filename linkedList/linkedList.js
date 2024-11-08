c658a4c689bf353fa845d01a3e437588c832b708
ashinjoy <ashinjoy22@gmail.com>
2024-11-08 19:18:07 +0530

    linked list operations optimized

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -41,7 +41,7 @@ class LinkedList {
     let curr = this.head;
     let list = "";
     if (this.isEmpty()) {
-      return 'linked list is empty'
+      return "linked list is empty";
     }
     while (curr) {
       list += curr.value + " ";
@@ -85,38 +85,37 @@ class LinkedList {
     current.next = node;
     node.next = temp;
   }
-  deletFromBegining(){
-    if(this.isEmpty()) return
-    this.head = this.head.next
-    return
+  deletFromBegining() {
+    if (this.isEmpty()) return;
+    this.head = this.head.next;
+    return;
   }
-  deleteFromEnd(){
-    if(this.isEmpty()) return 
-    if(this.head.next === null){ 
-      this.head = null
-      return
+  deleteFromEnd() {
+    if (this.isEmpty()) return;
+    if (this.head.next === null) {
+      this.head = null;
+      return;
     }
-    let current = this.head
-    while(current && current.next.next){
-      current = current.next
+    let current = this.head;
+    while (current && current.next.next) {
+      current = current.next;
     }
-    current.next = null
+    current.next = null;
   }
-  deleteFromPosition(pos){
-    if(this.isEmpty()) return 
-    if(pos === 1) {
-      this.deletFromBegining()
-      return
+  deleteFromPosition(pos) {
+    if (this.isEmpty()) return;
+    if (pos === 1) {
+      this.deletFromBegining();
+      return;
     }
-      let currPos = 1
-      let current = this.head
-    while( currPos < pos-1){
-      current =  current.next
-      currPos++
-    } 
-    let deleteElement = current.next
-    let temp = deleteElement.next
-    current.next = temp
+    let currPos = 1;
+    let current = this.head;
+    while (current.next && currPos < pos - 1) {
+      current = current.next;
+      currPos++;
+    }
+    if (!current.next) return;
+    current = current.next.next
   }
 }
 
@@ -126,14 +125,7 @@ linkedList.prepend("zizou");
 linkedList.prepend("kiran");
 linkedList.prepend("shephin");
 linkedList.prepend("ashin");
-// linkedList.insertElement(5, "shainy");
-// linkedList.deletFromBegining()
-// linkedList.deleteFromEnd()
-// linkedList.deleteFromEnd()
-// linkedList.deleteFromEnd()
-// linkedList.deleteFromEnd()
-linkedList.deleteFromPosition(1)
+linkedList.deleteFromPosition(1);
 
 const list = linkedList.print();
 console.log(list);
-
